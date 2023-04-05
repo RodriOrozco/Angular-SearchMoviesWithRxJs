c2cb7d45564b3fb3c2015b9050a3298f65f681d1
Rodrigo Orozco <orozcorodri8@gmail.com>
2023-04-05 20:02:20 -0300

    project created and components added, also i create services folder and interface folder

---
+++ src/app/components/card-movie/card-movie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-movie',
+  templateUrl: './card-movie.component.html',
+  styleUrls: ['./card-movie.component.less']
+})
+export class CardMovieComponent {
+
+}
